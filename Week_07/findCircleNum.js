a1569ad63677a3a34999df3d284de9ec83780c23
BaiXue <18258809774@163.com>
2020-10-18 21:40:25 +0800

    朋友圈

---
+++ Week_07/findCircleNum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} M
+ * @return {number}
+ */
+var findCircleNum = function(M) {
+  const visited = Array.from({ length: M.length }).fill(0)
+  let res = 0
+
+  for (let i = 0; i < visited.length; i++) {
+    if (!visited[i]) {
+      visited[i] = i
+      dfs(i)
+      res++
+    }
+  }
+
+  return res
+
+  function dfs(i) {
+    for (let j = 0; j < M.length; j++) {
+      if (i !== j && !visited[j] && M[i][j]) {
+        visited[j] = 1
+        dfs(j)
+      }
+    }
+  }
+};
