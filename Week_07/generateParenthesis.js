e6c32e649718c89cf8ab8ecce532902a5504e5a8
BaiXue <18258809774@163.com>
2020-10-18 21:48:15 +0800

    括号生成

---
+++ Week_07/generateParenthesis.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  let res = []
+
+  function generate(str, left, right) {
+    if (str.length === 2 * n) {
+      res.push(str)
+      return
+    }
+
+    if (left > 0) generate(str + '(', left - 1, right)
+    if (right > left) generate(str + ')', left, right - 1)
+  }
+
+  generate('', n, n)
+  return res
+};
